3c8c8d5e3e67905833869b8ab9119d8287e6df63
volodya <volodya@maill.volodya>
2020-04-15 15:44:09 +0300

    Updates

---
+++ precache-manifest.64c748e5bf5193e37ef27a7206ea4542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93d74b3adaf289b05cd749e55fdc632",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "996df992f7f5f8b0fb03",
+    "url": "/covid-19/static/css/main.592d5bf8.chunk.css"
+  },
+  {
+    "revision": "a02a780b5bc62042d36b",
+    "url": "/covid-19/static/js/2.9005b655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.9005b655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996df992f7f5f8b0fb03",
+    "url": "/covid-19/static/js/main.039612db.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19/static/media/logo.5d5d9eef.svg"
+  }
+]);
